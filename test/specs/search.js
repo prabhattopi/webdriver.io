fa879ba8fb380775d38869d4f0d3ace60b2a13a1
prabhattopi <topi9864@gmail.com>
2022-11-30 19:25:48 +0530

    The file is updated  with many test cases

---
+++ test/specs/search.js
@@ -0,0 +1,26 @@
+describe("Ebay Product Search", () => {
+  it("should open the url and verify the title", () => {
+    browser.url("https://www.ebay.com/");
+    expect(browser).toHaveTitle(
+      "Electronics, Cars, Fashion, Collectibles &amp; More | eBay"
+    );
+  });
+  it("Should search for a product and verify the search text value", () => {
+    //always try with unique selectors
+    const searchInput = $("#gh-ac");
+    const searchButton = $("#gh-btn");
+
+    searchInput.addValue("Laptop");
+    searchButton.click();
+
+    //assertion
+    expect(searchInput).toHaveValue("Laptop");
+  });
+  it("should redirect to a new page and verify the title",()=>{
+    expect(browser).toHaveTitle("Laptop for sale | eBay")
+  })
+  it("search category should be updated",()=>{
+    const searchCategory=$("#gh-cat option:nth-child(1)")
+    expect(searchCategory).toHaveText("PC Laptops & Netoks")
+  })
+});
---
+++ test/specs/watches.js
@@ -0,0 +1,21 @@
+describe("watches", () => {
+  it("show the banner container", () => {
+    browser.url("https://www.ebay.com/b/Wristwatches/31387/bn_2408451");
+    const promoBanner = $("div.title-banner__right-image");
+    expect(promoBanner).toBeDisplayed();
+  });
+  it("Banner Title", () => {
+    const infoTitle = $("h1.title-banner__title");
+    expect(infoTitle).toHaveTextContaining("Wristwatch");
+  });
+  it("should contain link on list data button and verify", () => {
+    const shopButton = $(".b-visualnav__tile:nth-child(1)");
+    expect(shopButton).toHaveLinkContaining("/Casio/");
+    expect(shopButton).toBeClickable();
+  });
+  it("should click the shop button and verify new url",()=>{
+    const shopButton = $(".b-visualnav__tile:nth-child(1)");
+   shopButton.click();
+   expect(browser).toHaveUrl("https://www.ebay.com/b/Wristwatches/31387/bn_2408451")
+  })
+});
