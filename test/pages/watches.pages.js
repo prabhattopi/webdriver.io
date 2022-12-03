ce42f7e5b879df6d41b0c8bc0f41f8a3728fad96
prabhattopi <topi9864@gmail.com>
2022-12-03 17:57:07 +0530

    The file is updated with 3755 findElements of array

--- test/pages/watches.pages.js
+++ test/pages/watches.pages.js
@@ -1,26 +1,31 @@
 import Page from "./page";
 
-
-
 class WatchesPage extends Page {
-  
   get infoTitle() {
     return $("h1.title-banner__title");
   }
   get SearchBtn() {
     return $("#gh-btn");
-    
-
   }
-  get shopButton(){
-    return $(".b-visualnav__tile:nth-child(1)")
+  get shopButton() {
+    return $(".b-visualnav__tile:nth-child(1)");
   }
-  get category(){
-    return $("#gh-cat option:nth-child(1)")
-    // return $$("#gh-cat option:nth-child(1)")
+  get category() {
+    // return $("#gh-cat option:nth-child(1)")//find Element
+    return $$("#gh-cat option:nth-child(1)")[0]; //find Elements with arrays
   }
+   get watchesCategoryList(){
+    return $$('section[id="s0-17-12_2-0-1[0]-0-0"] ul li')//array of Elements
+   }
+   
   open() {
-   super.open("https://www.ebay.com/b/Wristwatches/31387/bn_2408451");
+    super.open("https://www.ebay.com/b/Wristwatches/31387/bn_2408451");
+  }
+  async getWatchesCategoryListText(){//array of Elements
+    const watchesList=[]
+  await this.watchesCategoryList
+  await this.watchesCategoryList.map(async(e)=>watchesList.push(await e.getText()))
+  return watchesList
   }
 }
 export default new WatchesPage();
--- test/specs/watches.js
+++ test/specs/watches.js
@@ -2,40 +2,45 @@
 
 import { expect as chaiExpect } from "chai";
 import watchesPages from "../pages/watches.pages";
-import WatchesPage from '../pages/watches.pages'
+import WatchesPage from "../pages/watches.pages";
 describe("watches", () => {
+  // after(async ()=>{
+  //   await browser.url('https://www.ebay.com')
+  // })
 
+  //  afterEach(()=>{
+  //   browser.refresh()
+  //  })
 
+  before(async () => {
+    WatchesPage.open();
+  });
 
-
-  after(async ()=>{
-    await browser.url('https://www.ebay.com')
-  })
-
-   afterEach(()=>{
-    browser.refresh()
-   })
-
-
-  it("show the banner container", async() => {
-    watchesPages.open()
+  it("should verify the watches category List", async () => {
+    const watchesCategoryList = await WatchesPage.getWatchesCategoryListText();
+    chaiExpect(watchesCategoryList).to.deep.equal([
+      "Watches",
+      "Wristwatches",
+      "Other Watches",
+      "Pocket Watches",
+    ]);
+  });
+  it("show the banner container", async () => {
+    watchesPages.open();
     const promoBanner = $("div.title-banner__right-image");
     await expect(promoBanner).toBeDisplayed();
   });
-  it("Banner Title", async() => {
-    
+  it("Banner Title", async () => {
     await expect(WatchesPage.infoTitle).toHaveTextContaining("Wrist");
   });
-  it("should contain link on list data button and verify", async() => {
- 
+  it("should contain link on list data button and verify", async () => {
     await expect(WatchesPage.shopButton).toHaveLinkContaining("/Casio/");
     await expect(watchesPages.shopButton).toBeClickable();
   });
-  it("should click the shop button and verify new url", async() => {
- 
+  it("should click the shop button and verify new url", async () => {
     await watchesPages.shopButton.click();
     const url = await browser.getUrl();
-    await  chaiExpect(url).to.include("/Casio-Watches/");
+    chaiExpect(url).to.include("/Casio-Watches/");
 
     await expect(browser).toHaveUrl(
       "https://www.ebay.com/b/Casio-Watches/31387/bn_2973204"
